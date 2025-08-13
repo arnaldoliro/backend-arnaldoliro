c5db3b0ac2243ed271d88ebf2c2bec95793e38ae
Arnaldo Liro <dinholiro@gmail.com>
2025-08-13 09:28:32 -0300

    feat: creating a resource for sending emails

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,13 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('form')
   getHello(): string {
     return this.appService.getHello();
+  }
+  
+  @Get()
+  getHello2(): string {
+    return this.appService.getHello2();
   }
 }
